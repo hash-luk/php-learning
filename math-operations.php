76d17ec1e7c62953e19955d967e5bb0e213f15d2
hash-luk <lucas.patrick2506@gmail.com>
2023-08-29 22:27:15 -0300

    Learning math operations

---
+++ math-operations.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$adicao = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$doisAoCubo = 2 ** 3;
+$restoDaDivisao = 10 % 3;
+
+echo "Adiçao = " .$adicao;
+echo "\nSubtração = " .$subtracao;
+echo "\nMultiplicação = " .$multiplicacao;
+echo "\nDivisão = " .$divisao;
+echo "\nDois ao cubo = " .$doisAoCubo;
+echo "\nResto da divisão = " .$restoDaDivisao;
