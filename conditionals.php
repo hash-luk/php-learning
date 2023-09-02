459ba3d9ddbbed3a2cf18b3c5c50a9b76010efc6
hash-luk <lucas.patrick2506@gmail.com>
2023-09-01 21:34:15 -0300

    Learning conditionals

--- conditionals.php
+++ conditionals.php
@@ -1,12 +1,15 @@
 <?php
 
-$idade = 20;
-$nome = "Lucas";
+$idade = 17;
+$acompanhantes = 1;
 
 echo "Você só pode entrar se tiver a partir de 18 anos\n" . PHP_EOL;
 
-if($idade >= 18 AND $nome == "Lucas") 
-    echo "Você tem mais de 18 anos, pode entrar!";
+if($idade >= 18 || ($idade >= 16 && $acompanhantes >= 1)) 
+    if($idade >= 18)
+        echo "Você tem 18 anos ou mais, pode entrar!";
+    else 
+        echo "Você possui {$idade} anos e está acompanhado(a), pode entrar!";
 else 
     echo "Você só tem {$idade} anos. Você não pode entrar!\n";
 
