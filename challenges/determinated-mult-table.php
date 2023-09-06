c51305dc4ea52181cd7bf9013f61434bab38e4c9
hash-luk <lucas.patrick2506@gmail.com>
2023-09-05 22:24:40 -0300

    Solving basic php challenges

--- challenges/determinated-mult-table.php
+++ challenges/determinated-mult-table.php
@@ -3,9 +3,9 @@
 
 do {
     $number = readline("Write the number to show the table:");
-} while(is_numeric(is_int($number)));
+} while(!is_numeric($number));
 
 
-for ($i = 0; $i <= 9; $i++) {
+for ($i = 0; $i <= 10; $i++) {
     echo "$number x $i = " . $number * $i . PHP_EOL;
 }
