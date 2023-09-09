e638deb402daa9af1f688967137c6c30e2fe80b8
hash-luk <lucas.patrick2506@gmail.com>
2023-09-08 21:09:38 -0300

    Learning Arrays of arrays

--- arrays.php
+++ arrays.php
@@ -3,4 +3,23 @@
 $idadeList = [18, 30, 25, 21, 22, 23, 26, 19];
 $idade = $idadeList[3];
 
-echo $idade;
+$conta1 = [
+    "titular" => "Lucas",
+    "saldo" => 1000
+];
+
+$conta2 = [
+    "titular" => "Vinicius",
+    "saldo" => 2000
+];
+
+$conta3 = [
+    "titular" => "André",
+    "saldo" => 1900
+];
+
+$contasCorrentes = [$conta1, $conta2, $conta3];
+
+for($i = 0; $i < count($contasCorrentes); $i++) {
+    echo $contasCorrentes[$i]["titular"] . PHP_EOL;
+}
