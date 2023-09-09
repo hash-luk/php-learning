70bc7d1119d4088b84befcec9ce934ed7ab6eb60
hash-luk <lucas.patrick2506@gmail.com>
2023-09-08 22:34:04 -0300

    Learning about functions, and type parameters

--- bank.php
+++ bank.php
@@ -1,16 +1,32 @@
 <?php
 
-function showMessage($message) {
+function showMessage(string $message) {
     echo $message . PHP_EOL;
 }
 
-function withdraw($account, $value) {
+function withdraw(array $account, float $value):array 
+{
 
     if($value > $account["saldo"]) {
         showMessage("{$account['titular']} não possui saldo suficiente para saque!");
     } else {
         $account["saldo"] -= $value;
     }
+
+    return $account;
+}
+
+function deposit(array $account, float $value):array 
+{
+    if($value > 0) {
+        $account["saldo"] += $value;
+
+        showMessage("Depósito para {$account['titular']} realizado com sucesso!");
+    } else {
+        showMessage("O valor do depósito precisa ser maior que 0!");
+    }
+
+
 
     return $account;
 }
@@ -31,6 +47,7 @@ $contasCorrentes = [
 ];
 
 $contasCorrentes["123.456.789-10"] = withdraw($contasCorrentes["123.456.789-10"], 1050);
+$contasCorrentes["145.987.327-64"] = deposit($contasCorrentes["145.987.327-64"], -1050);
 
 foreach ($contasCorrentes as $cpf => $conta) {
     showMessage("$cpf - {$conta['titular']} - {$conta['saldo']}");
