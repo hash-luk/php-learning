b5c6a000f9ec87c5706ef40ca4b47880244abcc5
hash-luk <lucas.patrick2506@gmail.com>
2023-09-09 00:19:32 -0300

    Learning about include and require

--- bank.php
+++ bank.php
@@ -1,35 +1,6 @@
 <?php
 
-function showMessage(string $message) {
-    echo $message . PHP_EOL;
-}
-
-function withdraw(array $account, float $value):array 
-{
-
-    if($value > $account["saldo"]) {
-        showMessage("{$account['titular']} não possui saldo suficiente para saque!");
-    } else {
-        $account["saldo"] -= $value;
-    }
-
-    return $account;
-}
-
-function deposit(array $account, float $value):array 
-{
-    if($value > 0) {
-        $account["saldo"] += $value;
-
-        showMessage("Depósito para {$account['titular']} realizado com sucesso!");
-    } else {
-        showMessage("O valor do depósito precisa ser maior que 0!");
-    }
-
-
-
-    return $account;
-}
+require_once 'funcoes.php';
 
 $contasCorrentes = [
     '123.456.789-10' => [
