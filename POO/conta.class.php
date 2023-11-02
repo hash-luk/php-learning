83f14b5a7a823fdf5ab9da566c1e7b6b005202a0
hash-luk <lucas.patrick2506@gmail.com>
2023-11-02 11:26:37 -0300

    Learning POO

--- POO/conta.class.php
+++ POO/conta.class.php
@@ -4,4 +4,32 @@ class Conta {
     public $cpfTitular;
     public $nomeTitular;
     public $saldo;
+
+    public function sacar(float $valorSacar):void {
+        if($valorSacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        } 
+        
+        $this->saldo -= $valorSacar;
+    }
+
+    public function depositar(float $valorDeposito):void {
+        if($valorDeposito <= 0) {
+            echo "Valor insuficiente para depósito";
+            return;
+        } 
+
+        $this->saldo += $valorDeposito;  
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino):void {
+        if($valorATransferir > $this->saldo) {
+            echo "Valor solicitado, ultrapassa o saldo disponível na conta";
+            return;
+        } 
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir); 
+    }
 }
